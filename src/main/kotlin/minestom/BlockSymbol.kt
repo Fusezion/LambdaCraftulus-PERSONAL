10945af4acba40093d0db605cac49df10b286199
mibers <woodrow.conran@gmail.com>
2025-03-27 22:18:39 +0000

    reorganize and add parsing for ingame blocks

---
+++ src/main/kotlin/minestom/BlockSymbol.kt
@@ -0,0 +1,31 @@
+package me.chriss99.minestom
+
+import net.minestom.server.instance.block.Block
+
+data class BlockSymbol(val namespace: String, val symbol: String)
+
+val blockSymbols = listOf(
+    BlockSymbol("minecraft:diamond_block", "!"),
+    BlockSymbol("minecraft:sticky_piston", "("),
+    BlockSymbol("minecraft:piston", ")"),
+    BlockSymbol("minecraft:sea_lantern", "\\"),
+    BlockSymbol("minecraft:stone", "."),
+    BlockSymbol("minecraft:red_wool", "a"),
+    BlockSymbol("minecraft:green_wool", "b"),
+    BlockSymbol("minecraft:blue_wool", "c"),
+)
+
+private val blockToSymbolMap = blockSymbols.associate { it.namespace to it.symbol }
+private val symbolToBlockMap = blockSymbols.associate { it.symbol to it.namespace }
+
+fun getParsableLambdaSymbol(block: Block): String {
+    return blockToSymbolMap[block.namespace().toString()] ?: ""
+}
+
+fun getPrettyLambdaSymbol(block: Block): String {
+    return getParsableLambdaSymbol(block).replace("\\", "λ")
+}
+
+fun getBlockFromSymbol(symbol: String): Block? {
+    return Block.fromNamespaceId(symbolToBlockMap[symbol] ?: "")
+}
--- src/main/kotlin/minestom/LambdaDisplayLogic.kt
+++ src/main/kotlin/minestom/LambdaDisplayLogic.kt
@@ -1,13 +1,18 @@
 package me.chriss99.minestom
 
+import me.chriss99.lambda.lazyReduce
+import me.chriss99.parse.lex
+import me.chriss99.parse.parse
 import net.kyori.adventure.text.Component
 import net.minestom.server.coordinate.BlockVec
+import net.minestom.server.coordinate.Point
 import net.minestom.server.coordinate.Vec
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.EntityType
 import net.minestom.server.entity.metadata.display.TextDisplayMeta
 import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.event.player.PlayerBlockBreakEvent
+import net.minestom.server.event.player.PlayerBlockInteractEvent
 import net.minestom.server.event.player.PlayerBlockPlaceEvent
 import net.minestom.server.instance.Instance
 import net.minestom.server.instance.block.Block
@@ -17,20 +22,13 @@ import java.util.*
 fun lambdaDisplayLogic(eventHandler: GlobalEventHandler) {
     eventHandler.addListener(PlayerBlockPlaceEvent::class.java) { event ->
         event.isCancelled = true
+        event.blockFace
         val instance = event.instance
-        val below = event.blockPosition.sub(0, 1, 0)
-        if (instance.getBlock(below) != Block.BLACK_STAINED_GLASS) {
+        val clicked = event.blockPosition.sub(event.blockFace.toDirection().vec())
+        if (instance.getBlock(clicked) != Block.BLACK_STAINED_GLASS) {
             return@addListener
         }
-
-        val block = getLambdaBlock(event.block)
-
-        val symbol = createLambdaSymbol(block, event.blockPosition, instance)
-
-        val linked = block.withTag(Tag.UUID("link"), symbol)
-
-        instance.setBlock(below, linked)
-
+        setLambdaBlock(event.block, clicked, instance)
     }
 
     eventHandler.addListener(PlayerBlockBreakEvent::class.java) { event ->
@@ -40,16 +38,53 @@ fun lambdaDisplayLogic(eventHandler: GlobalEventHandler) {
 
         instance.setBlock(event.blockPosition, Block.BLACK_STAINED_GLASS)
     }
+
+    eventHandler.addListener(PlayerBlockInteractEvent::class.java) { event ->
+        event.isCancelled = true
+        val instance = event.instance
+        var target = event.blockPosition
+        if (event.block.name() == "minecraft:diamond_block") {
+
+            var expression = ""
+            var i = 0
+            while (instance.getBlock(target) != Block.BLACK_STAINED_GLASS) {
+                i++
+                if (i > 100) {
+                    return@addListener
+                }
+                target = target.add(1, 0, 0)
+                expression += getParsableLambdaSymbol(instance.getBlock(target))
+            }
+            println(expression)
+            val reduced = lazyReduce(parse(lex(expression)))
+            event.player.sendMessage(reduced.toString())
+        }
+    }
 }
 
-fun createLambdaSymbol(block: Block, pos: BlockVec, instance: Instance): UUID {
+fun setLambdaBlock(block: Block, clicked: Point, instance: Instance) {
+
+    val symbol = createLambdaSymbol(block, clicked, instance)
+    val lambdaBlock = getLambdaBlock(block)
+        .withTag(
+            Tag.UUID("link"),
+            symbol
+        )
+
+    instance.setBlock(clicked, lambdaBlock)
+}
+
+fun createLambdaSymbol(block: Block, clicked: Point, instance: Instance): UUID {
     val display = Entity(EntityType.TEXT_DISPLAY)
     val meta = display.entityMeta as TextDisplayMeta
-    meta.text = getLambdaSymbol(block)
+    meta.text = Component.text(getPrettyLambdaSymbol(block))
     meta.scale = Vec(7.0,7.0,7.0)
     meta.isShadow = true
-    meta.backgroundColor = 0x00000000
-    display.setInstance(instance, pos.add(0.0,0.5,0.0))
+    meta.backgroundColor = 0x0000000
+    meta.isHasNoGravity = true
+
+    display.setInstance(instance, clicked.add(1.0,1.0,0.0))
+    display.setView(-180f, 0f)
     return display.uuid
 }
 
@@ -59,18 +94,4 @@ fun getLambdaBlock(block: Block): Block {
         Block.PISTON -> Block.PISTON.withProperty("facing", "west")
         else -> block
     }
-}
-
-fun getLambdaSymbol(block: Block): Component {
-    return Component.text(when (block.namespace().toString()) {
-        "minecraft:diamond_block" -> "!"
-        "minecraft:sticky_piston" -> "("
-        "minecraft:piston" -> ")"
-        "minecraft:glowstone" -> "λ"
-        "minecraft:stone" -> "."
-        "minecraft:red_wool" -> "a"
-        "minecraft:green_wool" -> "b"
-        "minecraft:blue_wool" -> "c"
-        else -> ""
-    })
 }
