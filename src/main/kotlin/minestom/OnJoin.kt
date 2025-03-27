7ec9b504b3c3fe3f43d53d61b172106649b70558
mibers <woodrow.conran@gmail.com>
2025-03-27 22:19:27 +0000

    add default items to joining players

--- src/main/kotlin/minestom/OnJoin.kt
+++ src/main/kotlin/minestom/OnJoin.kt
@@ -1,5 +1,7 @@
 package minestom
 
+import me.chriss99.minestom.blockSymbols
+import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.GameMode
 import net.minestom.server.entity.PlayerSkin
@@ -7,6 +9,9 @@ import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerSkinInitEvent
 import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+import net.minestom.server.timer.TaskSchedule
 
 fun onJoin(eventHandler: GlobalEventHandler, instanceContainer: InstanceContainer) {
     eventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
@@ -14,6 +19,14 @@ fun onJoin(eventHandler: GlobalEventHandler, instanceContainer: InstanceContaine
         event.spawningInstance = instanceContainer
         player.respawnPoint = Pos(0.0, 41.0, 0.0)
         player.gameMode = GameMode.CREATIVE
+
+        MinecraftServer.getSchedulerManager().scheduleTask({
+            blockSymbols.forEach { blockSymbol ->
+                val material = Material.fromNamespaceId(blockSymbol.namespace) ?: Material.AIR
+                val item = ItemStack.of(material)
+                player.inventory.addItemStack(item)
+            }
+        }, TaskSchedule.tick(30), TaskSchedule.stop())
     }
 
     eventHandler.addListener(PlayerSkinInitEvent::class.java) { event ->
