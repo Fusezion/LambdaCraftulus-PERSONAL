b05d429ea30a6f339af5c7571fe423426b548cd2
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-03-28 14:45:07 +0100

    rewrite StringLexer.kt

--- src/main/kotlin/parse/StringLexer.kt
+++ src/main/kotlin/parse/StringLexer.kt
@@ -1,13 +1,28 @@
 package me.chriss99.parse
 
 fun lex(source: String): List<Token> {
-    return source.map { char ->
-        when (char) {
-            '\\' -> Token.Lambda
-            '.' -> Token.Dot
-            '(' -> Token.LParen
-            ')' -> Token.RParen
-            else -> if (char.isLetter()) Token.Var(char.toString()) else throw IllegalArgumentException()
-        }
-    }
+    return source.map { stringToToken(it.toString()) ?: throw IllegalArgumentException("Unknown character: \"$it\"") }
+}
+
+private val tokens = listOf(
+    Token.Lambda to "\\",
+    Token.Dot to ".",
+    Token.LParen to "(",
+    Token.RParen to ")"
+)
+
+private val charToTokenMap = tokens.associate { it.second to it.first }
+private val tokenToCharMap = tokens.associate { it.first to it.second }
+
+val stringToToken = { string: String ->
+    if (string.fold(true) { a, b -> a && b.isLetter() })
+        Token.Var(string)
+    else
+        charToTokenMap[string]
+}
+val tokenToString = { token: Token ->
+    if (token is Token.Var)
+        token.name
+    else
+        tokenToCharMap[token]
 }
