3ba3477bfea32c3d2caf6b712029bb7c3f9cf1e9
mibers <woodrow.conran@gmail.com>
2025-03-28 23:43:17 +0000

    remake LambdaSymbolManager to be more rebose

--- src/main/kotlin/minestom/LambdaDisplayLogic.kt
+++ src/main/kotlin/minestom/LambdaDisplayLogic.kt
@@ -2,11 +2,15 @@ package me.chriss99.minestom
 
 import me.chriss99.lambda.lazyReduce
 import me.chriss99.minestom.LambdaParser.parseBlocks
+import me.chriss99.minestom.LambdaSymbolManager.createErrorSymbol
 import me.chriss99.parse.ParsingException
 import me.chriss99.parse.lex
 import me.chriss99.parse.parse
 import minestom.BASE_BLOCK
+import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
+import net.minestom.server.MinecraftServer
+import net.minestom.server.color.Color
 import net.minestom.server.coordinate.Point
 import net.minestom.server.coordinate.Vec
 import net.minestom.server.entity.Entity
@@ -20,6 +24,7 @@ import net.minestom.server.instance.block.Block
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
 import net.minestom.server.tag.Tag
+import net.minestom.server.timer.TaskSchedule
 import java.util.*
 
 class LambdaEventHandler(private val eventHandler: GlobalEventHandler, private val blockManager: LambdaBlockManager) {
@@ -91,16 +96,34 @@ class LambdaBlockManager {
 
 object LambdaSymbolManager {
     fun createLambdaSymbol(block: Block, clicked: Point, instance: Instance): UUID {
-        val display = Entity(EntityType.TEXT_DISPLAY)
-        val meta = display.entityMeta as TextDisplayMeta
+        val (meta, display) = createSymbol()
         meta.text = Component.text(getPrettyLambdaSymbol(block))
-        meta.scale = Vec(7.0, 7.0, 0.01)
-        meta.isShadow = true
-        meta.backgroundColor = 0x0000000
-        meta.isHasNoGravity = true
-        meta.isSeeThrough = true
-        display.setInstance(instance, clicked.add(1.0, 1.0, -0.01))
+        display.setInstance(instance, clicked.add(0.65, 1.0, -0.01))
         display.setView(-180f, 0f)
         return display.uuid
+    }
+
+    fun createErrorSymbol(symbol: String, point: Point, instance: Instance): UUID {
+        val (meta, display) = createSymbol()
+        meta.text = Component.text(symbol)
+        display.setInstance(instance, point.add(0.65, 1.0, -0.01))
+        display.setView(-180f, 0f)
+
+        MinecraftServer.getSchedulerManager().buildTask {
+            display.remove()
+        }.delay(TaskSchedule.seconds(5)).schedule()
+
+        return display.uuid
+    }
+
+    private fun createSymbol(): Pair<TextDisplayMeta, Entity> {
+        val display = Entity(EntityType.TEXT_DISPLAY)
+        val meta = display.entityMeta as TextDisplayMeta
+        meta.scale = Vec(6.0, 6.0, 0.01)
+        meta.isShadow = true
+        meta.backgroundColor = 0x00000000
+        meta.isHasNoGravity = true
+        meta.isSeeThrough = true
+        return Pair(meta, display)
     }
 }
